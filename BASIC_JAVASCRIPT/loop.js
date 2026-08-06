7e9757674f3e24e3d7823c1cc2b40ac9d7058477
Shaurya0117 <shaurya7185@gmail.com>
2026-08-06 10:56:18 +0530

    loops in JS

---
+++ BASIC_JAVASCRIPT/loop.js
@@ -0,0 +1,22 @@
+for(let i =1;i<=10,i++){
+    console.log(i);
+}
+
+let i =1;
+while(i<11){
+    console.log(i);
+    i++;
+}
+
+let i =1;
+do{
+    console.log(i);
+    i++;
+} while(i<15);
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=i;i++){
+        console.log(i);
+    }
+    
+}
