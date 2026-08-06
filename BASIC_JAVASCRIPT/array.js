6726c337c4308968886fc3b1b3f6ca27699503a6
Shaurya0117 <shaurya7185@gmail.com>
2026-08-06 11:59:47 +0530

    array in JS

---
+++ BASIC_JAVASCRIPT/array.js
@@ -0,0 +1,18 @@
+arr = [1,22,2,3,4];
+console.log(arr);
+//length
+n = arr.length;
+console.log(n)
+
+//adding new element from back
+arr.push(8);
+console.log(arr);
+//deletion last element
+arr.pop()
+console.log(arr);
+//adding in front
+arr.unshift(12)
+console.log(arr);
+//remove first
+arr.shift();
+console.log(arr);
--- BASIC_JAVASCRIPT/loop.js
+++ BASIC_JAVASCRIPT/loop.js
@@ -1,22 +1,32 @@
-for(let i =1;i<=10,i++){
-    console.log(i);
-}
+// for(let i =1;i<=10,i++){
+//     console.log(i);
+// }
 
-let i =1;
-while(i<11){
-    console.log(i);
-    i++;
-}
+// let i =1;
+// while(i<11){
+//     console.log(i);
+//     i++;
+// }
 
-let i =1;
-do{
-    console.log(i);
-    i++;
-} while(i<15);
+// let i =1;
+// do{
+//     console.log(i);
+//     i++;
+// } while(i<15);
 
-for(let i=1;i<=5;i++){
-    for(let j=1;j<=i;i++){
-        console.log(i);
-    }
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=i;i++){
+//         console.log(i);
+//     }
     
+// }
+ob = [1,2,3,45,5];
+
+for (const ele of ob) {
+    console.log(ele);
 }
+
+ob.forEach(element => {
+    console.log(ele);
+    
+});
