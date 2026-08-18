c75e3c2d476f17417934e601c8e7222580ebefa9
Shaurya0117 <shaurya7185@gmail.com>
2026-08-18 11:32:59 +0530

    array operation

--- BASIC_JAVASCRIPT/array.js
+++ BASIC_JAVASCRIPT/array.js
@@ -1,18 +1,41 @@
-arr = [1,22,2,3,4];
-console.log(arr);
-//length
-n = arr.length;
-console.log(n)
+let arr = [1, 22, 2, 3, 4];
 
-//adding new element from back
+console.log(arr);
+
+
+// length
+let n = arr.length;
+console.log(n);
+
+
+// adding new element from back
 arr.push(8);
 console.log(arr);
-//deletion last element
-arr.pop()
+
+
+// deletion of last element
+arr.pop();
 console.log(arr);
-//adding in front
-arr.unshift(12)
+
+
+// adding in front
+arr.unshift(12);
 console.log(arr);
-//remove first
+
+
+// remove first element
 arr.shift();
 console.log(arr);
+
+
+// slice()
+let b = arr.slice(1, 4);
+console.log("After slice(1, 4):", b);
+
+console.log("Original array:", arr);
+
+
+
+// splice()
+arr.splice(2, 1);
+console.log("After splice(2, 1):", arr);
