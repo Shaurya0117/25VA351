b5ab6286342912406904d3d58cf8f896dfd5d339
Shaurya0117 <shaurya7185@gmail.com>
2026-08-18 11:49:50 +0530

    String method

---
+++ BASIC_JAVASCRIPT/string.js
@@ -0,0 +1,17 @@
+console.log("A string is a sequence of characters used to store text.")
+
+let str = "Hello World";
+
+console.log(str.length);        // length
+
+console.log(str.toUpperCase()); // HELLO WORLD
+
+console.log(str.toLowerCase()); // hello world
+
+console.log(str.charAt(0));     // H
+
+console.log(str.indexOf("World")); // 6
+
+console.log(str.lastIndexOf("o"));
+
+console.log(str.slice(0, 5));   // Hello
