ad119ce34922caca4b286305d70d49626d14405f
Shaurya0117 <shaurya7185@gmail.com>
2026-09-03 11:36:38 +0530

    arrow function

---
+++ BASIC_JAVASCRIPT/arrowfun.js
@@ -0,0 +1,13 @@
+console.log(" An arrow function is a shorter way to write a function  ")
+
+
+// function add(a, b) {   //normaL
+//     return a + b;
+// }
+
+// const add = (a, b) => {   //Arrow Function
+//     return a + b;
+// };
+
+const add = (a, b) =>  `${a + b}`;
+console.log (add(9,12));
