1ecf79bcec5738478832d03286561b5442db78cc
Shaurya0117 <shaurya7185@gmail.com>
2026-09-03 11:50:46 +0530

    pattern printing

---
+++ BASIC_JAVASCRIPT/patternprinting.js
@@ -0,0 +1,19 @@
+// for (let i = 1; i <= 5; i++) {
+//     let pattern = "";
+
+//     for (let j = 1; j <= i; j++) {
+//         pattern += "*";
+//     }
+
+//     console.log(pattern);r
+// }
+
+for (let i = 1; i <= 5; i++) {
+    let row= "";
+
+    for (let j = 1; j <= i; j++) {
+        row += j;
+    }
+
+    console.log(row);
+}
